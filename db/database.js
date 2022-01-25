d4cae6b78b50435bfc212563ef8740fe5107062d
svitlana-snappy <svitlana.shymko@snappy.com>
2022-01-25 03:57:20 +0200

    fix path for db

--- db/database.js
+++ db/database.js
@@ -2,7 +2,7 @@ const { Sequelize } = require("sequelize");
 
 const sequelize = new Sequelize('test-db', 'user', 'pass', {
     dialect: 'sqlite',
-    host: './dev.sqlite',
+    host: './db/dev.sqlite',
 })
 
 module.exports = sequelize;
